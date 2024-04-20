1562af6243bd78666bb72edfc5d3f2fa0b0fb255
Sinedvolzok <sinedvolzok@gmail.com>
2024-04-20 18:46:17 +0300

    Initial Commit

---
+++ Books/BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksApp.swift
+//  Books
+//
+//  Created by Denis Kozlov on 20.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
